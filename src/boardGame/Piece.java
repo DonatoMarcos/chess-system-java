ccb78990153980fd43d2aaca06fc4959f3a35999
edbmdds <marcos.donato.da.silva@ericsson.com>
2023-05-16 12:11:42 -0300

    Chess layer and printing the board

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -1,5 +1,8 @@
 package boardGame;
 
+// This is a generic class. It represents a piece with a position on a given board.
+// Which game this piece belongs to? - I don't know.
+// Which type of piece within the game this piece is? - I don't know.
 public class Piece {
 	
 	protected Position position;
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,24 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Piece;
+
+// This class is an extension over the general Piece clas and, indicates
+// that we'll have a piece for the chess game.
+// On top of general board and position, we'll have the color, which may
+// be white or black.
+// Now, which piece type am I? I don't know yet.
+public class ChessPiece extends Piece {
+	
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+
+}
