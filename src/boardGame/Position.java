f892f3c1a7c40da7ff448cc007a3cee18a898e06
edbmdds <marcos.donato.da.silva@ericsson.com>
2023-05-16 10:47:13 -0300

    Start to implement board piece

--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -4,6 +4,13 @@ public class Position {
 	
 	private int row;
 	private int column;
+	
+	public Position(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
 	public int getRow() {
 		return row;
 	}
@@ -16,15 +23,9 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	public Position(int row, int column) {
-		super();
-		this.row = row;
-		this.column = column;
-	}
-	
+
 	@Override
 	public String toString() {
 		return row + ", " + column;
 	}
-
 }
